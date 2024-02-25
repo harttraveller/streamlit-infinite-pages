c0ec42ea827f48f0b63a2f054b1750e854733580
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 18:13:42 -0500

    sip/core: update

--- sip/core.py
+++ sip/core.py
@@ -1,3 +1,4 @@
+import os
 from sip.schema import AppConfig, Page
 from sip.constant import run_mode_environment_key
 
@@ -8,8 +9,13 @@ class App:
         for key, val in modes.items():
             if not isinstance(val, AppConfig):
                 raise ValueError(f"'{key}' must be instance of 'AppConfig'")
-        self.modes = modes
+        self.modes: dict[str, AppConfig] = modes
 
     def add(self, page: Page) -> None: ...
 
-    def build(self) -> None: ...
+    def build(self) -> None:
+        run_mode: str | None = os.getenv(run_mode_environment_key)
+        if run_mode is None:
+            raise ValueError(f"'{run_mode_environment_key}' not set")
+        if run_mode not in self.modes.keys():
+            raise ValueError(f"'{run_mode}' not found in 'modes'")
