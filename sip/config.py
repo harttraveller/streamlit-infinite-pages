2f8f580d90b9ef1ddec1c831ec19959178991752
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 15:20:10 -0500

    sip/config: update

--- sip/config.py
+++ sip/config.py
@@ -1,56 +1,8 @@
-import streamlit as st
 from pathlib import Path
 from typing import Any, Optional, Callable
 from pydantic.dataclasses import dataclass
 from pydantic import field_validator
-from sip.defaults import default_unauthorized, default_undeveloped
 from sip.constant import path_default_logo, path_default_theme
-
-
-@dataclass
-class PageConfig:
-    # todo: finish docstring
-    """
-    _summary_
-
-    Args:
-        id (str): The unique page identifier.
-        title (Optional[str]): The page title to display as Heading 1 on page. Defaults to None.
-        main (Optional[Callable]): _description_. Defaults to 'undefined_page_callable'.
-        main_args (Optional[list[Any]]): _description_. Defaults to empty list.
-        main_kwargs (Optional[dict[str, Any]]): _description_. Defaults to empty dictionary.
-        show (Optional[Callable]): _description_. Defaults to None.
-        show_args (Optional[list[Any]]): _description_. Defaults to empty list.
-        show_kwargs (Optional[dict[str, Any]]): _description_. Defaults to empty dictionary.
-    """
-    id: str
-    title: Optional[str] = None
-    main: Optional[Callable[[Optional[Any]], None]] = None
-    main_args: Optional[list[Any]] = list()
-    main_kwargs: Optional[dict[str, Any]] = dict()
-    authorizer: Optional[Callable[[Optional[Any]], bool]] = None
-    authorizer_args: Optional[list[Any]] = list()
-    authorizer_kwargs: Optional[dict[str, Any]] = dict()
-    unauthorized: Optional[Callable[[Optional[Any]], None]] = default_unauthorized
-    unauthorized_args: Optional[list[Any]] = list()
-    unauthorized_kwargs: Optional[dict[str, Any]] = dict()
-    undeveloped: Optional[Callable[[Optional[Any]], None]] = default_undeveloped
-    undeveloped_args: Optional[list[Any]] = list()
-    undeveloped_kwargs: Optional[dict[str, Any]] = dict()
-
-    def __call__(self) -> Any:
-        if self.title is not None:
-            st.markdown(f"# {self.title}")
-        if self.main is None:
-            self.undeveloped(*self.undeveloped_args, **self.undeveloped_kwargs)
-        else:
-            if self.authorizer is None:
-                self.main(*self.main_args, **self.main_kwargs)
-            else:
-                if self.authorizer(*self.authorizer_args, **self.authorizer_kwargs):
-                    self.main(*self.main_args, **self.main_kwargs)
-                else:
-                    self.unauthorized(*self.unauthorized_args, **self.unauthorized_kwargs)
 
 
 @dataclass
@@ -59,9 +11,9 @@ class AppConfig:
     app_icon: str = "📚"  # * validated
     page_layout: str = "wide"  # * validated
     initial_sidebar_state: str = "expanded"  # * validated
-    custom_logo: str | Path = path_default_logo
-    custom_css: Optional[str | Path] = path_default_theme
-    custom_js: Optional[str | Path] = None
+    custom_logo: str | Path = path_default_logo  # * validated
+    custom_css: Optional[str | Path] = path_default_theme  # * validated
+    custom_js: Optional[str | Path] = None  # * validated
     initial_session_state: Optional[dict[str, Any]] = dict()
     authorization_function: Optional[Callable[[Optional[Any]], bool]] = None
     alpha_sort_pages: bool = False
