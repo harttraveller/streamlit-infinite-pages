f6da9321bebc1b2364f7199e013bab89ec2e27ec
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 18:13:35 -0500

    sip/constant: add default key

--- sip/constant.py
+++ sip/constant.py
@@ -10,3 +10,8 @@ path_streamlit_credentials: Path = path_streamlit / "credentials.toml"
 path_assets: Path = Path(__file__).parent / ".assets"
 path_default_theme: Path = path_assets / "default_theme.css"
 path_default_logo: Path = path_assets / "default_logo.png"
+
+
+# Variables
+
+run_mode_environment_key: str = "SIP_RUN_MODE"
