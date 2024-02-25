ca427b22b3c2a3fdab4cd50f91bc800119f0e7c2
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 18:51:09 -0500

    sip/env: update variables

--- sip/env.py
+++ sip/env.py
@@ -15,7 +15,10 @@ path_default_logo: Path = path_assets / "default_logo.png"
 # Variables
 
 run_mode_environment_key: str = "SIP_RUN_MODE"
+
+key_custom_css: str = "custom_css"
+key_custom_js: str = "custom_js"
 required_session_state_keys: set[str] = {
-    "custom_css",
-    "custom_js",
+    key_custom_css,
+    key_custom_js,
 }
