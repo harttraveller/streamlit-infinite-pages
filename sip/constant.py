cceb9c8420f673cf0d1589c6e9d149bf57072d9a
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 16:06:50 -0500

    sip/constant: update

--- sip/constant.py
+++ sip/constant.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 # Paths
 
+path_home: Path = Path.home()
+path_streamlit: Path = path_home / ".streamlit"
+path_streamlit_config: Path = path_streamlit / "config.toml"
 path_assets: Path = Path(__file__).parent / ".assets"
 path_default_theme: Path = path_assets / "default_theme.css"
 path_default_logo: Path = path_assets / "default_logo.png"
