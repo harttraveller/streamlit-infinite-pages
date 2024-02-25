179173e1e68492f50d5b1e18cdfa1e52d8549d34
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 14:58:37 -0500

    sip/constant: update

--- sip/constant.py
+++ sip/constant.py
@@ -6,8 +6,3 @@ from pathlib import Path
 path_assets: Path = Path(__file__).parent / ".assets"
 path_default_theme: Path = path_assets / "default_theme.css"
 path_default_logo: Path = path_assets / "default_logo.png"
-
-# Variables
-
-default_unauthorized_message: str = "You are not authorized to access this page."
-default_undeveloped_messaged: str = "This page has not yet been developed."
