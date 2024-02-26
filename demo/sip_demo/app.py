b0474a23ccf0c89a19d747a5b6553dca3ed75b30
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 20:56:38 -0500

    demo/sip_demo/app: update

--- demo/sip_demo/app.py
+++ demo/sip_demo/app.py
@@ -1,1 +1,34 @@
+from sip.core import App, AppConfig, Page
+from .page import home, test, broken
 
+
+dev_config: AppConfig = AppConfig(disable_traceback=False)
+prod_config: AppConfig = AppConfig(disable_traceback=True)
+
+app = App(dev=dev_config, prod=prod_config)
+
+app.add(
+    Page(
+        id="home",
+        name="Home Page",
+        render_main=home.render,
+    )
+)
+
+app.add(
+    Page(
+        id="test",
+        name="Test Page",
+        render_main=test.render,
+    )
+)
+
+app.add(
+    Page(
+        id="broken",
+        name="Broken Page (Zero Division Error)",
+        render_blocked=broken.render,
+    )
+)
+
+app.build()
