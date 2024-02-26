7668bcbfc6b1db6caf97780a9fdcdd49629f37a4
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 20:10:35 -0500

    sip/core: refactor

--- sip/core.py
+++ sip/core.py
@@ -16,31 +16,45 @@ class App:
         for key, val in modes.items():
             if not isinstance(val, AppConfig):
                 raise ValueError(f"'{key}' must be instance of 'AppConfig'")
-        self.cfg: AppConfig = modes[run_mode]
+        self.config: AppConfig = modes[run_mode]
+        self.pages: dict[str, Page] = dict()
+        self.indexed_pages: list[str] = list()
 
-    def add(self, page: Page) -> None: ...
+    def add(self, page: Page) -> None:
+        self.pages[page.id] = page
+        if page.is_accessible:
+            self.indexed_pages.append(page.id)
 
     def __set_page_config(self) -> None:
         st.set_page_config(
-            page_title=self.cfg.app_name,
-            page_icon=self.cfg.app_icon,
-            layout=self.cfg.page_layout,
-            initial_sidebar_state=self.cfg.initial_sidebar_state,
+            page_title=self.config.app_name,
+            page_icon=self.config.app_icon,
+            layout=self.config.page_layout,
+            initial_sidebar_state=self.config.initial_sidebar_state,
         )
+
+    def __control_exception_traceback(self) -> None:
+        if self.config.disable_traceback:
+            backend.modify_exception_behavior(
+                exception_handler=self.config.exception_handler
+            )
 
     def __initialize_session_state(self) -> None:
         "initialize streamlit session state keys"
         backend.add_session_state_variables(data=env.required_session_state_keys)
-        backend.add_session_state_variables(data=self.cfg.initial_session_state)
+        backend.add_session_state_variables(data=self.config.initial_session_state)
 
     def __apply_custom_css(self) -> None:
-        css: str = backend.load_css(self.cfg.custom_css_path)
+        css: str = backend.load_css(self.config.custom_css_path)
         backend.inject_css(css)
 
     def __apply_custom_js(self) -> None:
-        js: str = backend.load_js(self.cfg.custom_js_path)
+        js: str = backend.load_js(self.config.custom_js_path)
         backend.inject_js(js)
 
     def build(self) -> None:
         self.__set_page_config()
+        self.__control_exception_traceback()
         self.__initialize_session_state()
+        self.__apply_custom_css()
+        self.__apply_custom_js()
