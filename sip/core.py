b35e218037751a903fea4388aeeaaa12d6ead921
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 19:33:02 -0500

    sip/core: update

--- sip/core.py
+++ sip/core.py
@@ -1,67 +1,16 @@
 import os
 import streamlit as st
-from streamlit.commands.page_config import Layout
-from streamlit.delta_generator import DeltaGenerator
-from typing import Any, Optional, Callable
-from pydantic.dataclasses import dataclass
 from sip.config.app import AppConfig
-from sip.env import run_mode_environment_key
-
-
-def default_unauthorized(*args, **kwargs) -> None:
-    st.error("You are not authorized to access this page.")
-
-
-def default_undeveloped(*args, **kwargs) -> None:
-    st.error("This page has not yet been developed.")
-
-
-@dataclass
-class Page:
-    # todo: finish docstring
-    """
-    _summary_
-
-    Args:
-        id (str): The unique page identifier.
-        title (Optional[str]): The page title to display as Heading 1 on page. Defaults to None.
-        main (Optional[Callable]): _description_. Defaults to 'undefined_page_callable'.
-        main_args (Optional[list[Any]]): _description_. Defaults to empty list.
-        main_kwargs (Optional[dict[str, Any]]): _description_. Defaults to empty dictionary.
-        show (Optional[Callable]): _description_. Defaults to None.
-        show_args (Optional[list[Any]]): _description_. Defaults to empty list.
-        show_kwargs (Optional[dict[str, Any]]): _description_. Defaults to empty dictionary.
-    """
-    id: str
-    title: Optional[str] = None
-    main: Callable[[Optional[Any]], None] = default_undeveloped
-    main_args: list[Any] = list()
-    main_kwargs: dict[str, Any] = dict()
-    authorizer: Optional[Callable[[Optional[Any]], bool]] = None
-    authorizer_args: list[Any] = list()
-    authorizer_kwargs: dict[str, Any] = dict()
-    unauthorized: Callable[[Optional[Any]], None] = default_unauthorized
-    unauthorized_args: list[Any] = list()
-    unauthorized_kwargs: dict[str, Any] = dict()
-
-    def __call__(self) -> Any:
-        if self.title is not None:
-            st.markdown(f"# {self.title}")
-        if self.authorizer is None:
-            self.main(*self.main_args, **self.main_kwargs)
-        else:
-            if self.authorizer(*self.authorizer_args, **self.authorizer_kwargs):
-                self.main(*self.main_args, **self.main_kwargs)
-            else:
-                self.unauthorized(*self.unauthorized_args, **self.unauthorized_kwargs)
+from sip.schema import Page
+from sip import env, backend
 
 
 class App:
 
     def __init__(self, **modes) -> None:
-        run_mode: str | None = os.getenv(run_mode_environment_key)
+        run_mode: str | None = os.getenv(env.run_mode_environment_key)
         if run_mode is None:
-            raise ValueError(f"'{run_mode_environment_key}' not set")
+            raise ValueError(f"'{env.run_mode_environment_key}' not set")
         if run_mode not in modes.keys():
             raise ValueError(f"'{run_mode}' not found in key word arguments")
         for key, val in modes.items():
@@ -81,10 +30,16 @@ class App:
 
     def __initialize_session_state(self) -> None:
         "initialize streamlit session state keys"
-        if self.cfg.initial_session_state is not None:
-            for key, val in self.cfg.initial_session_state.items():
-                if key not in st.session_state.keys():
-                    st.session_state[key] = val
+        backend.add_session_state_variables(data=env.required_session_state_keys)
+        backend.add_session_state_variables(data=self.cfg.initial_session_state)
+
+    def __apply_custom_css(self) -> None:
+        css: str = backend.load_css(self.cfg.custom_css_path)
+        backend.inject_css(css)
+
+    def __apply_custom_js(self) -> None:
+        js: str = backend.load_js(self.cfg.custom_js_path)
+        backend.inject_js(js)
 
     def build(self) -> None:
         self.__set_page_config()
