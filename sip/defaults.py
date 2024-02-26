7f63c25c5286b148c8712e6bb11118784bb373a6
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-02-25 19:33:09 -0500

    sip/defaults: create

---
+++ sip/defaults.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def default_unauthorized(*args, **kwargs) -> None:
+    st.error("You are not authorized to access this page.")
+
+
+def default_undeveloped(*args, **kwargs) -> None:
+    st.error("This page has not yet been developed.")
+
+
+def default_exception_handler(*args, **kwargs) -> None:
+    st.toast(":red[Internal Server Error]")
