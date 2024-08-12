313354eeb278c6f90b056fa3a36531be6ce8099a
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-11 20:11:25 -0700

    sip/__init__: update

--- sip/__init__.py
+++ sip/__init__.py
@@ -1,21 +1,21 @@
 from importlib import metadata
 from .__meta__ import __name__, __package__, __module__, __version__
+from .core import App, Page
 
-streamlit_version: str = metadata.version("streamlit")
-compatible_streamlit_versions: set[str] = {
+__streamlit_version: str = metadata.version("streamlit")
+__compatible_streamlit_versions: set[str] = {
     "1.37.1",
 }
 
+if __streamlit_version not in __compatible_streamlit_versions:
 
-class IncompatibleStreamlitVersionError(Exception): ...
+    class IncompatibleStreamlitVersionError(Exception): ...
 
-
-if streamlit_version not in compatible_streamlit_versions:
     raise IncompatibleStreamlitVersionError(
         (
             "Due to some patches introduced to control traceback, "
             "it is uncertain whether streamlit versions outside of the "
-            f"supported versions will work. Your version is {streamlit_version}.\n"
-            f"Please change to one of the following versions: {compatible_streamlit_versions}"
+            f"supported versions will work. Your version is {__streamlit_version}.\n"
+            f"Please change to one of the following versions: {__compatible_streamlit_versions}"
         )
     )
